7f857d7d9f9795141e986d4e5e8828729342551d
yo2boy <yo2boyftw@gmail.com>
2014-11-09 22:37:29 -0500

    initial commit

---
+++ app/src/main/java/cps633/lab4/Lab4.java
@@ -0,0 +1,14 @@
+package cps633.lab4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/* Controller class to the user interface found under /res/layout/main */
+public class Lab4 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ app/src/main/java/cps633/lab4/SMSReceiver.java
@@ -0,0 +1,6 @@
+package cps633.lab4;
+
+/* Class used to listen for incoming SMS Messages */
+public class SMSReceiver{
+	
+}
